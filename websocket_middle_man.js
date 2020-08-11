bb8ceda2511c5fff1286fa714e17684a97d30c5a
IHASASUSHI <raathka@gmail.com>
2020-08-11 15:51:47 -0400

    skeleton update

--- websocket_middle_man.js
+++ websocket_middle_man.js
@@ -2,23 +2,44 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({
     port: 8080,
-    perMessageDeflate: {
-      zlibDeflateOptions: {
+      perMessageDeflate: {
+        zlibDeflateOptions: {
         // See zlib defaults.
-        chunkSize: 1024,
-        memLevel: 7,
-        level: 3
-      },
-      zlibInflateOptions: {
-        chunkSize: 10 * 1024
-      },
-      // Other options settable:
-      clientNoContextTakeover: true, // Defaults to negotiated value.
-      serverNoContextTakeover: true, // Defaults to negotiated value.
-      serverMaxWindowBits: 10, // Defaults to negotiated value.
-      // Below options specified as default values.
-      concurrencyLimit: 10, // Limits zlib concurrency for perf.
-      threshold: 1024 // Size (in bytes) below which messages
-      // should not be compressed.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 10, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024 // Size (in bytes) below which messages
+        // should not be compressed.
+      }
+});
+
+class Clients {
+    constructor() {
+        this.clients = {}
     }
-  });
+    saveClient(id, socket) {
+        this.clients[id] = socket
+    }
+}
+
+wss.on('connection', (client, req) => {
+    ip.push(req.socket.remoteAddress)
+})
+
+ws.on('message', (msg) => {
+    console.log()
+})
+
+ws.on('close', () => {
+    console.log("client closed connection")
+})
