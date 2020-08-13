ad660f020737a009f90b04af44308e84d2ab2935
Will Gascoigne <gcoigne@gmail.com>
2020-08-13 16:21:08 -0400

    Added input system
    SetLinearVelocity not working for some reason.

--- Physics/script.js
+++ Physics/script.js
@@ -11,9 +11,17 @@ var box2d = {
   b2DebugDraw : Box2D.Dynamics.b2DebugDraw
 };
 
+var input = {
+  left : false,
+  right : false,
+  down : false,
+  up : false,
+  m1 : false,
+  m2 : false,
+};
+
 class Player {
-  constructor(id, x, y) {
-    this.id = id;
+  constructor(x, y) {
     var bodyDef = new box2d.b2BodyDef();
     bodyDef.type = box2d.b2Body.b2_dynamicBody;
     bodyDef.position.x = x;
@@ -22,9 +30,13 @@ class Player {
     fixDef.density = 1;
     fixDef.friciton = 0.5;
     fixDef.restitution = 0.5;
-    fixDef.shape = new box2d.b2CircleShape(1);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    fixDef.shape = new box2d.b2CircleShape(15 / SCALE);
+    this.body = world.CreateBody(bodyDef);
+    this.fix = this.body.CreateFixture(fixDef);
     this.body = bodyDef;
+    this.speed = 30/SCALE;
+    this.velX = 0;
+    this.velY = 0;
   }
 }
 
@@ -34,21 +46,11 @@ var stage, world, player;
 function init() {
   stage = new createjs.Stage(document.getElementById("game-canvas"));
 
-  //let circle = new createjs.Shape();
-  //circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-  //circle.x = 100;
-  //circle.y = 100;
-  //stage.addChild(circle);
-  //stage.update();
-  stage.onMouseDown = () => {
-
-  }
   setupPhysics();
   stage.update();
   createjs.Ticker.on("tick", tick);
-  createjs.Ticker.setFPS(60);
   createjs.Ticker.useRAF = true;
-  player = new Player(0, 0, 0);
+  player = new Player(100 / SCALE, 50 / SCALE);
   stage.update();
 }
 
@@ -58,9 +60,9 @@ function setupPhysics() {
   fixDef.density = 1;
   fixDef.friciton = 0.5;
   var bodyDef = new box2d.b2BodyDef();
-  bodyDef.type = box2d.b2Body.b2_dynamicBody;
+  bodyDef.type = box2d.b2Body.b2_staticBody;
   bodyDef.position.x = 400 / SCALE;
-  bodyDef.position.y = 600 / SCALE;
+  bodyDef.position.y = 100 / SCALE;
   fixDef.shape = new box2d.b2PolygonShape();
   fixDef.shape.SetAsBox(400 / SCALE, 20 / SCALE);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
@@ -70,7 +72,6 @@ function setupPhysics() {
   debugDraw.SetDrawScale(SCALE);
   debugDraw.SetFlags(box2d.b2DebugDraw.e_shapeBit | box2d.b2DebugDraw.e_jointBit);
   world.SetDebugDraw(debugDraw);
-  //console.log(world.m_debugDraw);
 }
 
 function tick() {
@@ -78,7 +79,45 @@ function tick() {
   world.DrawDebugData();
   world.Step(1/60, 10, 10);
   world.ClearForces();
-  console.log(player.body.position.x)
+
+  let xInput = input.right - input.left;
+  let yInput = input.up - input.down;
+  let playerVel = new box2d.b2Vec2(xInput * player.speed, yInput * player.speed);
+  if (xInput != 0 && yInput != 0) {
+    playerVel.Multiply(0.707);
+  }
+  //player.body.SetLinearVelocity(playerVel);
 }
+
+function keyDown(key) {
+  console.log(key);
+  switch(key) {
+    case 'a':
+      input.left = true;
+    case 'd':
+      input.right = true;
+    case 's':
+      input.down = true;
+    case 'w':
+      input.up = true;
+  }
+}
+
+function keyUp(key) {
+  switch(key) {
+    case 'a':
+      input.left = false;
+    case 'd':
+      input.right = false;
+    case 's':
+      input.down = false;
+    case 'w':
+      input.up = false;
+  }
+}
+
+document.onkeydown = keyDown;
+document.onkeyup = keyUp;
+console.log(document.onkeydown);
 
 init();
