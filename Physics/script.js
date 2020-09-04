6299ff276ae3f4eb3af35cc413ad7b975c98b0e3
Will Gascoigne <gcoigne@gmail.com>
2020-09-04 17:55:10 -0400

    Created test level
    The transition just realoads the level.

--- Physics/script.js
+++ Physics/script.js
@@ -72,14 +72,43 @@ class MyContactListener extends Box2D.Dynamics.b2ContactListener {
           bodyDataB.endGrapple();
         }
       }
+      else if (bodyDataB instanceof Grapple) {
+	bodyDataB.isDone = true;
+      }
     }
 
     else if (bodyDataA instanceof Hazard) {
       if (bodyDataB instanceof Player) {
-        player.isDead = true;
+        bodyDataB.isDead = true;
       }
       else if (bodyDataB instanceof Grapple) {
-	grapple.isDone = true;
+	bodyDataB.isDone = true;
+      }
+    }
+
+    else if (bodyDataA instanceof Button) {
+      bodyDataA.numPressingObjects++;
+      if (bodyDataA.numPressingObjects == 1) {
+	bodyDataA.linkedObject.buttonStart();
+      }
+    }
+    
+    else if (bodyDataA instanceof Transition) {
+      bodyDataA.isTouched = true;
+    }
+
+    else if (bodyDataA instanceof Collectable) {
+      if (bodyDataB instanceof Player) {
+        if (!bodyDataA.isCollected) {
+          bodyDataA.isCollected = true;
+          type = bodyDataA.collectableType;
+          if (type in player.inventory) {
+            player.inventory[type]++;
+          }
+          else {
+            player.inventory[type] = 1;
+          }
+        }
       }
     }
     
@@ -90,6 +119,12 @@ class MyContactListener extends Box2D.Dynamics.b2ContactListener {
       else if (bodyDataB instanceof Player) {
         if (bodyDataB.isGrappling) {
           bodyDataB.endGrapple();
+        }
+      }
+      else if (bodyDataB instanceof Button) {
+        bodyDataB.numPressingObjects++;
+        if (bodyDataB.numPressingObjects == 1) {
+	  bodyDataB.linkedObject.buttonStart();
         }
       }
       else if (bodyDataB instanceof Grapple) {
@@ -116,7 +151,28 @@ class MyContactListener extends Box2D.Dynamics.b2ContactListener {
         }
       }
       else if (bodyDataB instanceof Hazard) {
-	    bodyDataA.isDead = true;
+	bodyDataA.isDead = true;
+      }
+      else if (bodyDataB instanceof Button) {
+        bodyDataB.numPressingObjects++;
+        if (bodyDataB.numPressingObjects == 1) {
+	  bodyDataB.linkedObject.buttonStart();
+        }
+      }
+      else if (bodyDataB instanceof Transition) {
+	bodyDataB.isTouched = true;
+      }
+      else if (bodyDataB instanceof Collectable) {
+        if (!bodyDataB.isCollected) {
+          bodyDataB.isCollected = true;
+          let type = bodyDataB.collectableType;
+          if (type in player.inventory) {
+            player.inventory[type]++;
+          }
+          else {
+            player.inventory[type] = 1;
+          }
+        }
       }
       else if (bodyDataB instanceof Box) {
         if (bodyDataA.isGrappling) {
@@ -129,26 +185,14 @@ class MyContactListener extends Box2D.Dynamics.b2ContactListener {
       if (bodyDataB instanceof Wall) {
         bodyDataA.hitObject = bodyDataB;
       }
+      else if (bodyDataB instanceof Door) {
+	bodyDataA.isDone = true;
+      }
       else if (bodyDataB instanceof Hazard) {
-	    bodyDataA.isDone = true;
+        bodyDataA.isDone = true;
       }
       else if (bodyDataB instanceof Box) {
         bodyDataA.hitObject = bodyDataB;
-      }
-    }
-
-    else if (bodyDataA instanceof Collectable) {
-      if (bodyDataB instanceof Player) {
-        if (!bodyDataA.isCollected) {
-          bodyDataB.isCollected = true;
-          type = bodyDataA.collectableType;
-          if (type in player.inventory) {
-            player.inventory[type]++;
-          }
-          else {
-            player.inventory[type] = 1;
-          }
-        }
       }
     }
   }
@@ -173,10 +217,28 @@ class MyContactListener extends Box2D.Dynamics.b2ContactListener {
       if (bodyDataB instanceof Ground) {
 	bodyDataA.isGrounded = false;
       }
+      else if (bodyDataB instanceof Button) {
+        bodyDataB.numPressingObjects--;
+        if (bodyDataB.numPressingObjects == 0) {
+	  bodyDataB.linkedObject.buttonEnd();
+        }
+      }
     }    
     else if (bodyDataA instanceof Player) {
       if (bodyDataB instanceof Ground) {
         bodyDataA.isGrounded = false;
+      }
+      else if (bodyDataB instanceof Button) {
+        bodyDataB.numPressingObjects--;
+        if (bodyDataB.numPressingObjects == 0) {
+	  bodyDataB.linkedObject.buttonEnd();
+        }
+      }
+    }
+    else if (bodyDataA instanceof Button) {
+      bodyDataA.numPressingObjects--;
+      if (bodyDataA.numPressingObjects == 0) {
+	bodyDataA.linkedObject.buttonEnd();
       }
     }
   }
@@ -199,6 +261,7 @@ class Ground {
     this.body = world.CreateBody(bodyDef);
     this.fix = this.body.CreateFixture(fixDef);
     this.body.SetUserData(this);
+    bodys.push(this.body);
   }
 }
 
@@ -216,6 +279,7 @@ class Wall {
     this.body = world.CreateBody(bodyDef);
     this.fix = this.body.CreateFixture(fixDef);
     this.body.SetUserData(this);
+    bodys.push(this.body);
   }
 }
 
@@ -234,11 +298,23 @@ class Door {
     this.fix = this.body.CreateFixture(fixDef);
     this.body.SetUserData(this);
     this.isOpen = false;
+    bodys.push(this.body);
+  }
+
+  buttonStart() {
+    this.isOpen = true;
+  }
+
+  buttonEnd() {
+    this.isOpen = false;
   }
 
   tick() {
     if (this.isOpen) {
       this.body.SetActive(false);
+    }
+    else {
+      this.body.SetActive(true);
     }
   }
 }
@@ -255,13 +331,67 @@ class Hazard {
     fixDef.shape = new box2d.b2PolygonShape();
     fixDef.shape.SetAsBox(w, h);
     fixDef.filter.categoryBits = categorys.STATIC;
-    fixDef.filter.maskBits = categorys.PLAYER || categorys.CREATURE;
-    fixDef.shape.SetAsBox(w, h);
-    fixDef.filter.categoryBits = categorys.STATIC;
-    fixDef.filter.maskBits = categorys.PLAYER | categorys.CREATURE;
     this.body = world.CreateBody(bodyDef);
     this.fix = this.body.CreateFixture(fixDef);
     this.body.SetUserData(this);
+    bodys.push(this.body);
+  }
+}
+
+class Button {
+  constructor(x, y, w, h, l) {
+    let bodyDef = new box2d.b2BodyDef();
+    bodyDef.type = box2d.b2Body.b2_staticBody;
+    bodyDef.position.x = x;
+    bodyDef.position.y = y;
+    let fixDef = new box2d.b2FixtureDef();
+    fixDef.isSensor = true;
+    fixDef.shape = new box2d.b2PolygonShape();
+    fixDef.shape.SetAsBox(w, h);
+    fixDef.filter.categoryBits = categorys.STATIC;
+    fixDef.filter.maskBits = categorys.INTERACT | categorys.PLAYER | categorys.CREATURE;
+    this.body = world.CreateBody(bodyDef);
+    this.fix = this.body.CreateFixture(fixDef);
+    this.linkedObject = l;
+    this.numPressingObjects = 0;
+    this.body.SetUserData(this);
+    bodys.push(this.body);
+  }
+}
+
+class Transition {
+  constructor(x, y, w, h, l) {
+    let bodyDef = new box2d.b2BodyDef();
+    bodyDef.type = box2d.b2Body.b2_staticBody;
+    bodyDef.position.x = x;
+    bodyDef.position.y = y;
+    let fixDef = new box2d.b2FixtureDef();
+    fixDef.isSensor = true;
+    fixDef.shape = new box2d.b2PolygonShape();
+    fixDef.shape.SetAsBox(w, h);
+    fixDef.filter.categoryBits = categorys.STATIC;
+    fixDef.filter.maskBits = categorys.PLAYER;
+    this.body = world.CreateBody(bodyDef);
+    this.fix = this.body.CreateFixture(fixDef);
+    this.levelIndex = l;
+    this.isTouched = false;
+    this.body.SetUserData(this);
+    bodys.push(this.body);
+  }
+
+  activate() {
+    for (let body of bodys) {
+      if (body) {
+       world.DestroyBody(body);
+      }
+    }
+    createLevel(this.levelIndex);
+  }
+
+  tick() {
+    if (this.isTouched) {
+      this.activate();
+    }
   }
 }
 
@@ -274,7 +404,7 @@ class Box {
     bodyDef.fixedRotation = true;
     bodyDef.linearDamping = 5;
     let fixDef = new box2d.b2FixtureDef();
-    fixDef.density = 0.5;
+    fixDef.density = 1;
     fixDef.friction = 0.5;
     fixDef.restitution = 0;
     fixDef.shape = new box2d.b2PolygonShape();
@@ -285,12 +415,39 @@ class Box {
     this.body.SetUserData(this);
     this.isGrounded = true;
     this.isDead = false;
+    bodys.push(this.body);
   }
   tick() {
     if (!this.isGrounded && (!player.grapple || player.grapple.hitObject != this)) {
       this.isDead = true;
     }
     if (this.isDead) {
+      this.body.SetActive(false);
+    }
+  }
+}
+
+class Collectable {
+  constructor(x, y, r, t) {
+    let bodyDef = new box2d.b2BodyDef();
+    bodyDef.type = box2d.b2Body.b2_staticBody;
+    bodyDef.position.x = x;
+    bodyDef.position.y = y;
+    let fixDef = new box2d.b2FixtureDef();
+    fixDef.shape = new box2d.b2CircleShape(r);
+    fixDef.filter.categoryBits = categorys.INTERACT;
+    fixDef.filter.maskBits = categorys.PLAYER;
+    fixDef.isSensor = true;
+    this.body = world.CreateBody(bodyDef);
+    this.fix = this.body.CreateFixture(fixDef);
+    this.body.SetUserData(this);
+    this.collectableType = t;
+    this.isCollected = false;
+    bodys.push(this.body);
+  }
+
+  tick() {
+    if (this.isCollected) {
       this.body.SetActive(false);
     }
   }
@@ -307,19 +464,20 @@ class Player {
     fixDef.density = 1;
     fixDef.friction = 0.5;
     fixDef.restitution = 0.25;
-    fixDef.shape = new box2d.b2CircleShape(20 / SCALE);
+    fixDef.shape = new box2d.b2CircleShape(32 / SCALE);
     fixDef.filter.categoryBits = categorys.PLAYER;
     this.body = world.CreateBody(bodyDef);
     this.fix = this.body.CreateFixture(fixDef);
     this.body.SetUserData(this);
-    this.moveImpulse = 2;
-    this.maxSpeed = 20;
+    this.moveImpulse = 8;
+    this.maxSpeed = 32;
     this.isGrappling = false;
     this.canGrapple = true;
     this.isDead = false;
     this.isGrounded = true;
     this.grapple = null;
     this.inventory = {};
+    bodys.push(this.body);
   }
 
   useGrapple() {
@@ -373,7 +531,7 @@ class Player {
 	    if (hitBody.GetType() == box2d.b2Body.b2_dynamicBody) {
               let grappleAngle = Math.atan2(hitCenter.y - playerCenter.y, hitCenter.x - playerCenter.x);
               let grappleImp = new box2d.b2Vec2(this.moveImpulse * Math.cos(grappleAngle), this.moveImpulse * Math.sin(grappleAngle));
-	      grappleImp.Multiply(2);
+              grappleImp.Multiply(2);
               this.body.ApplyImpulse(grappleImp, this.body.GetWorldCenter());
 	      grappleImp.Multiply(-1);
 	      hitBody.ApplyImpulse(grappleImp, hitBody.GetWorldCenter());
@@ -382,6 +540,7 @@ class Player {
 	    else {
               let grappleAngle = Math.atan2(grappleCenter.y - playerCenter.y, grappleCenter.x - playerCenter.x);
               let grappleImp = new box2d.b2Vec2(this.moveImpulse * Math.cos(grappleAngle), this.moveImpulse * Math.sin(grappleAngle));
+              grappleImp.Multiply(2);
               this.body.ApplyImpulse(grappleImp, this.body.GetWorldCenter());
 	    }
           }
@@ -434,43 +593,19 @@ class Grapple {
     this.body.SetLinearVelocity(v);
     this.hitObject;
     this.isDone = false;
+    bodys.push(this.body);
   }
 }
-
-class Collectable {
-  constructor(x, y, r, t) {
-    let bodyDef = new box2d.b2BodyDef();
-    bodyDef.type = box2d.b2Body.b2_staticBody;
-    bodyDef.position.x = x;
-    bodyDef.position.y = y;
-    let fixDef = new box2d.b2FixtureDef();
-    fixDef.shape = new box2d.b2CircleShape(r);
-    fixDef.filter.categoryBits = categorys.INTERACT;
-    fixDef.filter.maskBits = categorys.PLAYER;
-    fixDef.isSensor = true;
-    this.body = world.CreateBody(bodyDef);
-    this.fix = this.body.CreateFixture(fixDef);
-    this.body.SetUserData(this);
-    this.collectableType = t;
-    this.isCollected = false;
-  }
-
-  tick() {
-    if (this.isCollected) {
-      this.body.SetActive(false);
-    }
-  }
-}
-
 
 let SCALE = 30;
-let stage, world, contactListener, player, doors, boxes, collectables;
+let stage, world, contactListener, bodys, player, doors, transitions, collectables, boxes;
 
 function init() {
+  bodys = [];
   stage = new createjs.Stage(document.getElementById("game-canvas"));
   setupPhysics();
   setupInput();
-  createLevel();
+  createLevel(0);
   overlay.init(stage);
 
   createjs.Ticker.timingMode = createjs.Ticker.RAF;
@@ -507,15 +642,19 @@ function tick() {
     door.tick();
   }
 
-  for (let box of boxes) {
-    box.tick();
+  for (let transition of transitions) {
+    transition.tick();
   }
 
   for (let collectable of collectables) {
     collectable.tick();
   }
+
+  for (let box of boxes) {
+    box.tick();
+  }
   if(createjs.Ticker.getMeasuredFPS() < 50){
-    console.log(createjs.Ticker.getMeasuredFPS())
+    //console.log(createjs.Ticker.getMeasuredFPS())
   }
   overlay.tick(player);
 }
@@ -567,15 +706,24 @@ function setupInput() {
   });
 }
 
-function createLevel() {
-  new Wall(1920 / 2 / SCALE, 9 / SCALE, 300 / SCALE, 30 / SCALE, true);
-  new Hazard(50 / SCALE, 50 / SCALE, 30 / SCALE, 30 / SCALE);
-  player = new Player(1920 / 2 / SCALE, 1080 / 2 / SCALE);
-  doors = [new Door(200 / SCALE, 50 / SCALE, 60 / SCALE, 30 / SCALE)];
-  boxes = [new Box(1500 / SCALE, 1080 / 2 / SCALE, 30 / SCALE, 30 / SCALE)];
-  collectables = [new Collectable(100 / SCALE, 100 / SCALE, 10 / SCALE, "key")];
-  new Ground(1920 / 2 / SCALE, 1080 / 2 / SCALE, 120 / SCALE, 120 / SCALE);
-  new Ground(1500 / SCALE, 1080 / 2 / SCALE, 120 / SCALE, 120 / SCALE);
+function createLevel(l) {
+  switch(l) {
+    case 0:
+      player = new Player(240 / SCALE, 960 / SCALE);
+      new Ground(960 / SCALE, 960 / SCALE, 960 / SCALE, 160 / SCALE);
+      new Ground(1736 / SCALE, 192 / SCALE, 160 / SCALE, 160 / SCALE);
+      new Ground(180 / SCALE, 192 / SCALE, 160 / SCALE, 160 / SCALE);
+      new Wall(960 / SCALE, 32 / SCALE, 960 / SCALE, 32 / SCALE);
+      new Wall(960 / SCALE, 1048 / SCALE, 960 / SCALE, 32 / SCALE);
+      new Wall(32 / SCALE, 540 / SCALE, 32 / SCALE, 540 / SCALE);
+      new Wall(1888 / SCALE, 540 / SCALE, 32 / SCALE, 540 / SCALE);
+      new Wall(480 / SCALE, 384 / SCALE, 480 / SCALE, 32 / SCALE);
+      doors = [];
+      boxes = [];
+      collectables = [];
+      transitions = [new Transition(240 / SCALE, 192 / SCALE, 64 / SCALE, 64 / SCALE, 0)];
+      break;
+  }
 }
 
 init();
