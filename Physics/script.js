35346ca7b7416025b6a1e5d669fd85952d5ac28e
IHASASUSHI <raathka@gmail.com>
2020-09-04 23:21:11 -0400

    updates

--- Physics/script.js
+++ Physics/script.js
@@ -726,18 +726,18 @@ function loadLevel(l) {
       break;
     case 1:
       player = new Player(256 / SCALE, 960 / SCALE);
+      /*
       new Ground(480 / SCALE, 512 / SCALE, 480 / SCALE, 512 / SCALE);
       new Ground(1440 / SCALE, 256 / SCALE, 480 / SCALE, 256 / SCALE);
       new Ground(1440 / SCALE, 864 / SCALE, 480 / SCALE, 160 / SCALE);
-      /*
+      */
       new Wall(960 / SCALE, 32 / SCALE, 960 / SCALE, 32 / SCALE);
       new Wall(960 / SCALE, 1056 / SCALE, 960 / SCALE, 32 / SCALE);
       new Wall(32 / SCALE, 512 / SCALE, 32 / SCALE, 512 / SCALE);
       new Wall(1888 / SCALE, 512 / SCALE, 32 / SCALE, 512 / SCALE);
       new Wall(960 / SCALE, 576 / SCALE, 64 / SCALE, 448 / SCALE);
       new Wall(1728 / SCALE, 448 / SCALE, 240 / SCALE, 64 / SCALE);
-      new Wall(1472 / SCALE, 352 / SCALE, 64 / SCALE, 160 / SCALE);
-      */
+      new Wall(1472 / SCALE, 320 / SCALE, 64 / SCALE, 192 / SCALE);
       doors = [new Door(928 / SCALE, 96 / SCALE, 32 / SCALE, 32 / SCALE), new Door(1440 / SCALE, 96 / SCALE, 32 / SCALE, 32 / SCALE)];
       boxes = [new Box(480 / SCALE, 768 / SCALE, 32 / SCALE, 32 / SCALE), new Box(1440 / SCALE, 768 / SCALE, 32 / SCALE, 32 / SCALE)];
       new Button(480 / SCALE, 256 / SCALE, 32 / SCALE, 32 / SCALE, doors[0]);
